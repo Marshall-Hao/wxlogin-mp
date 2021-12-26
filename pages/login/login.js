4c594d2f485304b5cf36d8e466088ccdbc1c36c8
Marshallccc <Marshallchan666@outlook.com>
2021-12-26 22:56:05 +0800

    wx login frontend configuration

---
+++ pages/login/login.js
@@ -0,0 +1,89 @@
+// pages/login/login.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    hasUserInfo: false
+  },
+  getUserProfile() {
+    const page = this
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+          const userId = app.globalData.user.id
+          wx.request({
+            url: `${app.globalData.url}/users/${userId}`,
+            method: 'PUT',
+            data: {
+              userInfo: res.userInfo
+            },
+            success: (res) => {
+              page.setData({
+                user:res.data.currentUser,
+                hasUserInfo: true
+              }
+              )
+            }
+          })
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
